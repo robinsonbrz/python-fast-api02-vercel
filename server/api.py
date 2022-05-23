fefd57ab206514c29616e26614a70801d7affe55
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-23 00:19:14 -0300

    Readme e doc FastApi

--- server/api.py
+++ server/api.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from .routes import router as NoteRouter
 
-app = FastAPI()
+app = FastAPI(    
+    title="FastApi Hosted on Vercel",
+    description="Teste de API com deploy na vercel, CRUD inserção de notas sem persistência em BD",
+    version="0.0.1",
+    contact={
+        "name": "Robinson Enedino",
+        "url": "https://www.enedino.com.br",
+        "email": "robinsonbrz@gmail.com",
+        },
+    )
 
 
 @app.get("/", tags=["Root"])
 async def read_root() -> dict:
-    return {
-        "message": "Welcome to my notes application, use the /docs route to proceed"
-    }
+    # response = RedirectResponse(url='https://www.enedino.com.br')
+    response = RedirectResponse(url='/docs')
+    return response
 
 app.include_router(NoteRouter, prefix="/note")
